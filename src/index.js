7c2d450767e469d0b08a7ddd0d745128ee8fbfb9
nicholas <gonzaleznick5@gmail.com>
2023-10-06 14:05:57 -0400

    sessionCapture & browser info

--- src/index.js
+++ src/index.js
@@ -1,41 +1,74 @@
-// const API = require("./api");
 
-// class Minos {
-//   init(config = {}) {
-//     const api = new API(config);
-//     return api.init();
-//   }
-// }
-
-// module.exports = new Minos();
 
 const API = require("./api");
 const Ethers = require("./ethers");
 const InjectionLogging = require("./injectionLogger");
+const { v4: uuidv4  } = require('uuid');
+
 
 class Minos {
   constructor(config = {}) {
     this.api = new API(config);
     this.ethers = new Ethers(config);
+
     this.injectionLogging = null;
-    this.sessionId = this.generateSessionId();
+    this.sessionId = uuidv4();
+    // Automatically capture the browser information upon initialization
+    this.browserInfo = this.captureBrowserInfo();
 
+    }
+    
+    captureBrowserInfo() {
+        if (typeof window !== 'undefined') { // Ensure window object is available
+          const domain = window.location.hostname;
+          const path = window.location.pathname;
+          const userAgent = window.navigator.userAgent;
+
+          console.log("Captured Browser Info:", { domain, path, userAgent }); // Log the captured info
+    
+          return {
+            domain,
+            path,
+            userAgent
+          };
+        } else {
+          return {
+            domain: null,
+            path: null,
+            userAgent: null
+          };
+      }
   }
 
-  generateSessionId() {
-    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+ 
 
-  }
 
   // Initialize InjectionLogging with the user's provider
   initializeInjectionLogging(provider) {
-    this.injectionLogging = new InjectionLogging(provider, this.api.config.token, this.sessionId);
-  }
+    if (
+      this.browserInfo &&
+      this.browserInfo.domain &&
+      this.browserInfo.path &&
+      this.browserInfo.userAgent
+    ) {
+      this.injectionLogging = new InjectionLogging(
+        provider,
+        this.api.config.token,
+        this.sessionId,
+        this.browserInfo
+      );
+    } else {
+      console.error(
+        "Browser info is not present. InjectionLogging was not initialized."
+      );
+    }
+}
 
   logSessionData(level, message, context, userId, address) {
     const sessionId = this.sessionId;
-    this.api[level](message, context, userId, address, sessionId);
-  }
+    const browserInfo = this.browserInfo;
+    this.api[level](message, context, userId, address, sessionId, browserInfo);
+  } 
 
   fatal(message, context, userId, address) {
     return this.logSessionData("fatal", message, context, userId, address, this.sessionId);
@@ -50,7 +83,7 @@ class Minos {
   }
 
   info(message, context, userId, address) {
-    return this.logSessionData("info", message, context, userId, address, this.sessionId);
+    return this.logSessionData("info", message, context, userId, address, this.sessionId, this.browserInfo);
   }
 
   debug(message, context, userId, address) {
--- src/injectionLogger.js
+++ src/injectionLogger.js
@@ -3,7 +3,7 @@ const constants = require("./constants");
 const axios = require("axios");
 
 class InjectionLogging {
-  constructor(provider, token, sessionId) {
+  constructor(provider, token, sessionId, browserInfo) {
     this.provider = provider;
     this.originalPerform = provider.perform;
     this.baseURL = constants.MONITAUR_URL;
@@ -12,57 +12,91 @@ class InjectionLogging {
     this.axios.defaults.baseURL = this.baseURL;
     this.token = token;
     this.sessionId = sessionId;
+    this.browserInfo = browserInfo,
+      // Override the `perform` method
+      (provider.perform = async (method, params) => {
+        // Intercept function call and log it
+        // Intercept function call and log it
+        const decodedMethod = this.decodeMethod(method);
 
-    // Override the `perform` method
-    provider.perform = async (method, params) => {
-      // Intercept function call and log it
+        console.log("provider perform", decodedMethod.name, decodedMethod.args);
+        console.log("provider perform", method, params, this.browserInfo);
+        console.log("browserInfo", this.browserInfo);
+        try {
+          // Call the original `perform` method to send the RPC request
+          const result = await this.originalPerform.call(
+            provider,
+            method,
+            params
+          );
 
-      console.log("provider perform", method, params);
-      try {
-        // Call the original `perform` method to send the RPC request
-        const result = await this.originalPerform.call(
-          provider,
-          method,
-          params
-        );
+          const body ={
+            // userId: "userId",
+            // address: "address",
+            message: `Injection Logger Function result for ${method}`,
+            context: {
+              result: result,
+              method: method,
+              params: params,
+            },
+            sessionId: this.sessionId,
+            browserInfo: this.browserInfo,
+          }
+          console.log("body", body);
+          await this._request("info", body);
 
-        await this._request("info", {
-          // userId: "userId",
-          // address: "address",
-          message: `Injection Logger Function result for ${method}`,
-          context: {
-            result: result,
-            method: method,
-            params: params,
-          },
-          sessionId: this.sessionId,
-        });
+          return result;
+        } catch (error) {
+          await this._request("error", {
+            // userId: "userId",
+            // address: "address",
+            message: `Error calling ${method}`,
+            context: {
+              error: error,
+              method: method,
+              params: params,
+            },
+            sessionId: this.sessionId,
+            browserInfo: this.browserInfo,
+          });
 
-        return result;
-      } catch (error) {
-        await this._request("error", {
-          // userId: "userId",
-          // address: "address",
-          message: `Error calling ${method}`,
-          context: {
-            error: error,
-            method: method,
-            params: params,
-          },
-          sessionId: this.sessionId,
-        });
+          throw error;
+        }
+      });
+  }
 
-        throw error;
-      }
+  // Decode the method call to get the function name and arguments
+  decodeMethod(method) {
+    console.log("all method calls", method);
+    const parts = method.split(" ");
+    const name = parts[0];
+    const args = parts.slice(1);
+
+    return {
+      name: name,
+      args: args,
     };
   }
 
   async _request(level, data) {
+    if (
+      !this.browserInfo ||
+      !this.browserInfo.domain ||
+      !this.browserInfo.path ||
+      !this.browserInfo.userAgent
+    ) {
+      console.error("Browser info is missing. Log request aborted.");
+      return;
+    }
+    // console.log(this.browserInfo);
     const response = await this.axios.post("/create-event", {
       logLevel: level,
       ...data,
       token: this.token,
+      //   sessionId: this.sessionId,
+
     });
+    console.log(response.data);
 
     return response.data;
   }
